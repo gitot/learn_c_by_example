57b937a72c4634a536392e9f7b64f9d8e5038f57
guyot <1571337712@qq.com>
2021-04-30 22:21:03 +0800

    add int.c demo file

---
+++ int.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  int.c
+ *
+ *    Description:  int data type in c programming language demo
+ *
+ *        Version:  1.0
+ *        Created:  04/30/21 22:13:34
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  guyot (), guyotwang@gmail.com
+ *        Company:  xtransfer.cn
+ *
+ * =====================================================================================
+ */
+
+void demo_int ()
+{
+        printf("sizeof(int):%d\n",sizeof(int));
+	return ;
+}
+		
+int main ( int argc, char *argv[] )
+{
+        demo_int();
+	return 0;
+}				/* ----------  end of function main  ---------- */
